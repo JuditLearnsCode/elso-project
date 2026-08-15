7ba422661020184f694b3adf96ba780624fe0169
MouseWar <juditerdelyi@gmail.com>
2026-08-15 13:44:41 +0200

    Alapműveletek

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Sziasztok, ez egy Python program!")
 print("Ez a program bemutatja a Python alapjait.")
 
 print("Most néhány alapvető műveletet fogunk elvégezni.")
+
+print("Összeadás: 5 + 3 =", 5 + 3)
+print("Kivonás: 10 - 4 =", 10 - 4)
